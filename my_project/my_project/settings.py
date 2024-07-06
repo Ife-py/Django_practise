315c5dbb520cdd94ccb972928df5670f10be7a03
Ifeoluwa <omolekeifeoluwa2005@gmail.com>
2024-07-06 14:15:57 +0100

    Finished django module

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tm6(4rds@570scua7i8!g$@5f2m$gmlhi$k=y1u*t=b6_d2%wa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -118,14 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
 MEDIA_URL='media/'
 
-MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+STATIC_ROOT= BASE_DIR/ 'assets'
+MEDIA_ROOT= BASE_DIR/'media'
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- my_project/users/views.py
+++ my_project/users/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm #importing forms for new user creation
-
+from django.contrib.auth.forms import UserCreationForm ,AuthenticationForm #importing forms for new user creation
+from django.contrib.auth import login,logout
 # Create your views here.
 def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("posts:list")
-        else:
-            form =UserCreationForm()
+    else:
+        form =UserCreationForm()
     return render(request,"users/register.html", { "form": form })
+
+
+def login_view(request):
+    if request.method =="POST":
+        form = AuthenticationForm(data=request.POST) #kwargs means key word argument
+        if form.is_valid():
+            #LOGIN HERE
+            login(request, form.get_user())
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect("posts:list")
+    else:
+        form = AuthenticationForm()
+    return render(request,"users/login.html", { "form": form })
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("posts:list")
